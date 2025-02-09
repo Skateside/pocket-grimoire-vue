d19c6d5c6e57eb4a98106b560c38039d372dc0db
Skateside <sk85ide@hotmail.com>
2025-02-09 18:47:24 +0000

    Add an idea about managing the data

--- src/Game.vue
+++ src/Game.vue
@@ -1,28 +1,43 @@
 <template>
 
-    <div>
+    <div class="list">
         <button
-            v-for="role in store.roles"
+            v-for="role in globalStore.roles"
             :key="role.id"
+            type="button"
             @click="() => roleId = role.id"
         >{{ role.name }}</button>
     </div>
 
     <RoleToken v-if="roleId" :id="roleId" />
 
+    <hr>
+
+    <div class="list">
+        <button
+            v-for="infoToken in infoTokenStore.infoTokens"
+            :key="infoToken.id"
+            type="button"
+            @click="() => infoTokenId = infoToken.id"
+        >{{ infoToken.text }}</button>
+    </div>
+
 </template>
 
 <script lang="ts" setup>
     import { ref } from 'vue';
-    import { useGlobalStore } from "../src/store";
+    import { useInfoTokenStore, useGlobalStore } from "../src/store";
     import RoleToken from './components/RoleToken.vue';
 
-    const store = useGlobalStore();
-    const roleId = ref('');
+    const globalStore = useGlobalStore();
+    const roleId = ref("");
+
+    const infoTokenStore = useInfoTokenStore();
+    const infoTokenId = ref("");
 </script>
 
 <style lang="scss" scoped>
-div {
+.list {
     display: flex;
     flex-flow: row wrap;
     gap: 0.25em;
--- src/store.ts
+++ src/store.ts
@@ -1,4 +1,8 @@
+import type {
+    IInfoToken,
+} from "./scripts/types/data";
 import { defineStore } from "pinia"
+import { computed, ref, watch } from "vue";
 
 export const useGlobalStore = defineStore("global", {
 
@@ -13,5 +17,134 @@ export const useGlobalStore = defineStore("global", {
         },
 
     },
+
+});
+
+// Idea for the Info Tokens
+
+const storage = {
+
+    key: "pg",
+
+    getStored() {
+        return JSON.parse(localStorage.getItem(this.key) || "{}");
+    },
+
+    get<T = any>(key: string, defaultValue?: any) {
+        return (this.getStored()[key] ?? defaultValue) as T;
+    },
+
+    set(key: string, value: any) {
+
+        const stored = this.getStored();
+
+        stored[key] = value;
+        localStorage.setItem(this.key, JSON.stringify(stored));
+
+        return true;
+
+    }
+
+};
+
+export const useInfoTokenStore = defineStore("info-token", () => {
+
+    const STORAGE_KEY = "info-tokens";
+    const infoTokens = ref<IInfoToken[]>([
+        ...window.PG.infoTokens,
+        ...storage.get<IInfoToken[]>(STORAGE_KEY, []),
+    ]);
+
+    watch(infoTokens, (value) => {
+        storage.set(STORAGE_KEY, value.filter(({ isCustom }) => isCustom));
+    });
+
+    const getById = computed(() => (id: IInfoToken["id"]) => {
+        return infoTokens.value.find(({ id: infoTokenId }) => infoTokenId === id);
+    });
+
+    const byType = computed(() => {
+        return Object.groupBy(infoTokens.value, ({ isCustom }) => (
+            isCustom
+            ? "custom"
+            : "official"
+        ));
+    });
+
+    const add = (text: IInfoToken["text"]) => {
+
+        // TODO: randomId()
+        const infoToken: IInfoToken = {
+            id: `cit-${Math.random().toString(32).slice(2)}`,
+            text,
+            colour: "grey",
+            isCustom: true,
+        };
+
+        infoTokens.value.push(infoToken);
+
+    };
+
+    const getIndexOrDie = (id: IInfoToken["id"]) => {
+
+        const index = infoTokens.value.findIndex(({ id: infoTokenId }) => {
+            return infoTokenId === id;
+        });
+
+        if (index < 0) {
+            // console.warn("Unable to find Info Token with id '%s'", id);
+            // return;
+            throw new Error(id);
+        }
+
+        return index;
+
+    }
+
+    const getCustomIndex = (id: IInfoToken["id"]) => {
+
+        const index = getIndexOrDie(id);
+        const infoToken = infoTokens.value[index];
+
+        if (!infoToken.isCustom) {
+            // console.warn("Info Token '%s' is not custom and cannot be updated", id);
+            // return;
+            throw new Error(id);
+        }
+
+        return index;
+
+    };
+
+    const update = (id: IInfoToken["id"], text: IInfoToken["text"]) => {
+
+        const index = getCustomIndex(id);
+
+        infoTokens.value[index] = {
+            ...infoTokens.value[index],
+            text,
+        };
+
+    };
+
+    const remove = (id: IInfoToken["id"]) => {
+
+        const index = getCustomIndex(id);
+
+        infoTokens.value.splice(index, 1);
+
+    };
+
+    return {
+        // State.
+        infoTokens,
+        // Getters.
+        getById,
+        byType,
+        // Actions.
+        add,
+        update,
+        remove,
+    };
 
 });
